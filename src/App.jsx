aa5de6e842c85cc11947e722dd4a771ba27a44d1
LLosTy <marcojurgenjuras@gmail.com>
2024-09-17 15:17:20 +0200

    feat:Animal search completed, has it's own hook

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,50 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import {useEffect, useState} from 'react'
 import './App.css'
 
-function App() {
-  const [count, setCount] = useState(0)
+function useAnimalSearch() {
+    const search = async (q) => {
+        const response = await fetch (
+            'http://localhost:8080?' + new URLSearchParams({q})
+        )
+        const data = await response.json()
+        localStorage.setItem('lastQuery',q)
+        setAnimals(data)
+    }
 
-  return (
-    <>
-      <div>
+    const [animals, setAnimals] = useState([])
+
+    useEffect(() => {
+        const lastQuery = localStorage.getItem('lastQuery')
+        search(lastQuery)
+    }, []);
+
+    return { search, animals}
+}
+
+function Animal({type, name, age}) {
+    return (
+        <li>
+            <strong>{type}</strong> {name} ({age} years old)
+        </li>
+    )
+}
+
+function App() {
+
+    const {search, animals} = useAnimalSearch()
+
+    return (
+        <div>
+            <h1>Animal Farm</h1>
+            <input type="text" placeholder="search" onChange={(e) => search(e.target.value)}></input>
+          <ul>
+              {animals.map((animal) => (
+                  // <Animal key={animal.id} type={animal.type} name={animal.name} age={animal.age}/>
+                  <Animal key={animal.id} {...animal}/> //the same thing as above just different syntax
+              ))}
+              {animals.length === 0 && 'No Animals Found!'}
+          </ul>
       </div>
-    </>
   )
 }
 
